bb90a48e2592c4669e884755f01f8d16d1949481
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2023-07-04 15:20:27 +0200

    Get follow-up actions of reports from pod

--- data/challenge-report-index-template.md
+++ data/challenge-report-index-template.md
@@ -16,20 +16,4 @@ TODO: explain reports.
 Below you find the reports with follow-up actions for
 the Community Solid Server, Comunica, SolidLabLib.js, or other libraries/tools.
 
-### Community Solid Server
-
-None.
-
-### Comunica
-
-None.
-
-### SolidLabLib.js
-
-- [Greet user with preferred name](./greet-user-with-their-preferred-name.md)
-
-### Other
-
-- [Automatically test performance of each CSS commit](./automatically-test-performance-of-each-css-commit.md)
-- [External service stores calendar in pod](./external-service-stores-calendar-in-pod.md)
-- [Menubar app to check who is on vacation](./menubar-app-to-check-who-is-on-vacation.md)
+{{{LIST_ACTIONS}}}
--- data/download-reports.js
+++ data/download-reports.js
@@ -6,13 +6,23 @@
 const QueryEngine = require('@comunica/query-sparql').QueryEngine;
 const fs = require('fs-extra');
 const fetch = require('node-fetch-commonjs');
-const { exec } = require("child_process");
+const {exec} = require("child_process");
 
-main();
+const myEngine = new QueryEngine();
 
-async function main() {
-  const myEngine = new QueryEngine();
+generateMarkdownFile();
 
+async function generateMarkdownFile() {
+  const reports = await generateReportList();
+  const actions = await generateFollowUpActionsList();
+
+  let indexTemplate = await fs.readFile('challenge-report-index-template.md', 'utf8');
+  indexTemplate = indexTemplate.replace('{{{LIST_ALL_REPORTS}}}', reports);
+  indexTemplate = indexTemplate.replace('{{{LIST_ACTIONS}}}', actions);
+  fs.writeFile('../docs/challenge-reports/index.md', indexTemplate);
+}
+
+async function generateReportList() {
   const bindingsStream = await myEngine.queryBindings(`
   PREFIX schema: <http://schema.org/>
   SELECT * WHERE {
@@ -23,10 +33,7 @@ async function main() {
   });
 
   const bindings = await bindingsStream.toArray();
-  const markdown = generateList(bindings);
-  let indexTemplate = await fs.readFile('challenge-report-index-template.md', 'utf8');
-  indexTemplate = indexTemplate.replace('{{{LIST_ALL_REPORTS}}}', markdown);
-  fs.writeFile('../docs/challenge-reports/index.md', indexTemplate);
+  const markdown = generateReportListMarkdown(bindings);
 
   // Download Markdown files
   for (const binding of bindings) {
@@ -35,14 +42,111 @@ async function main() {
   }
 
   downloadImages();
+
+  return markdown;
 }
 
-function generateList(bindings) {
+async function generateFollowUpActionsList() {
+  const bindingsStream = await myEngine.queryBindings(`
+  PREFIX schema: <http://schema.org/>
+  PREFIX knows: <https://data.knows.idlab.ugent.be/person/office/#>
+  SELECT * WHERE {
+    ?action a knows:FollowUpAction;
+      knows:report ?report.
+      
+    ?report schema:name ?title.
+  
+    OPTIONAL {
+      ?action schema:object ?software.
+    } 
+  }`, {
+    sources: ['https://data.knows.idlab.ugent.be/person/office/solidlab-challenges'],
+  });
+
+  const bindings = await bindingsStream.toArray();
+  const markdown = generateFollowUpActionsListMarkdown(bindings);
+  return markdown;
+}
+
+function generateReportListMarkdown(bindings) {
   let markdown = '';
 
   for (const binding of bindings) {
     const localUrl = binding.get('report').id.replace('https://github.com/SolidLabResearch/Challenges/blob/main/reports', '.');
     markdown += `- [${binding.get('title').value}](${localUrl})\n`;
+  }
+
+  return markdown;
+}
+
+function generateFollowUpActionsListMarkdown(bindings) {
+  const comunica = [];
+  const css = [];
+  const solidlabLibJs = [];
+  const other = [];
+
+  for (const binding of bindings) {
+    const url = binding.get('report').id.replace('https://github.com/SolidLabResearch/Challenges/blob/main/reports', '.');
+    const title = binding.get('title').value;
+
+    if (binding.get('software')) {
+      const software = binding.get('software').id;
+
+      if (software === 'https://data.knows.idlab.ugent.be/person/office/software#solidlablib-js') {
+        solidlabLibJs.push({
+          url,
+          title
+        });
+      } else if (software === 'https://data.knows.idlab.ugent.be/person/office/software#comunica') {
+        comunica.push({
+          url,
+          title
+        });
+      } else if (software === 'https://data.knows.idlab.ugent.be/person/office/software#community-solid-server') {
+        css.push({
+          url,
+          title
+        });
+      } else {
+        other.push({
+          url,
+          title
+        });
+      }
+    } else {
+      other.push({
+        url,
+        title
+      });
+    }
+  }
+
+  let markdown = generateFollowUpActionSection(css, 'Community Solid Server');
+  markdown += generateFollowUpActionSection(comunica, 'Comunica');
+  markdown += generateFollowUpActionSection(solidlabLibJs, 'SolidLabLib.js');
+  markdown += generateFollowUpActionSection(other, 'Other');
+
+  return markdown;
+}
+
+function generateFollowUpActionSection(list, title) {
+  // Filter duplicate reports.
+  list = list.filter((value, index, self) =>
+      index === self.findIndex((t) => (
+        t.url === value.url
+      ))
+  );
+
+  let markdown = `### ${title}\n\n`;
+
+  if (list.length === 0) {
+    markdown += 'None.\n\n';
+  } else {
+    list.forEach(item => {
+      markdown += `- [${item.title}](${item.url})\n`
+    });
+
+    markdown += '\n';
   }
 
   return markdown;
